412c572b0bdf70ae181dc6af6ace0dbd4547ea0f
Amagash <tiffanysouterre@gmail.com>
2016-07-14 18:46:42 +0200

    merge

--- BioscreenAnalysis.R
+++ BioscreenAnalysis.R
@@ -4,11 +4,18 @@
 #Script to analyse Bioscreen data output
 
 library("ggplot2");
-library("gridExtra");
-library("dplyr");
-#library("plotrix");
 
 source("fonction.R")
+source("config.R")
+
+strainList <- getStrainList()
+mediumList <- getMediumList()
+inputFile <- getInputFile()
+
+input <- read.csv(inputFile, header=FALSE, dec = ".", stringsAsFactors=FALSE)
+
+init(input, strainList, mediumList)
+
 # 
 # input <- read.csv("./data/kdb.csv", header=FALSE, dec = ".", stringsAsFactors=FALSE)
 # strainList <- list("3936","4050","3986", "4097", "4096", "3700", "4160", "4255")
@@ -19,4 +26,5 @@ strainList <- list("3990","4136","4110", "4170", "4172", "4172", "4173", "4174",
 mediumList <- list("Base", "Base + ile", "Base + ile + suc", "Base + ile + nor")
 
 
-init(input, strainList, mediumList)
+
+# init(input, strainList, mediumList)
